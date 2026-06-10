e9ec21b417b0ab6b96c74f03ca36d32056b88b2e
suvadipmaparu79-cpu <suvadipmaparu79@gmail.com>
2026-06-10 08:19:57 +0530

    Implement tracking route for link clicks
    Added a tracking route to count clicks and redirect.

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+
+clicks = 0
 
 @app.route("/")
 def home():
     return "Video Tracker Running"
+
+@app.route("/t/<track_id>")
+def track(track_id):
+    global clicks
+    clicks += 1
+
+    print(f"Link opened: {track_id}")
+    print(f"Total clicks: {clicks}")
+
+    return redirect("https://google.com")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
