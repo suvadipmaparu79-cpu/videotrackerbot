4852c3f9270c5b1fe93fea9e60f965dbb8c22d78
suvadipmaparu79-cpu <suvadipmaparu79@gmail.com>
2026-06-10 08:33:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,14 @@ from flask import Flask, redirect
 
 app = Flask(__name__)
 
-clicks = 0
-
 @app.route("/")
 def home():
     return "Video Tracker Running"
 
 @app.route("/t/<track_id>")
 def track(track_id):
-    global clicks
-    clicks += 1
-
-    print(f"Link opened: {track_id}")
-    print(f"Total clicks: {clicks}")
-
-    return redirect("https://google.com")
+    print(f"Clicked: {track_id}")
+    return redirect("https://1024terabox.com/s/1lCVPToVChMA906byN79ERg")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
