5aed715015aebe0c5a7fa94c6cb2112bf81e72a3
suvadipmaparu79-cpu <suvadipmaparu79@gmail.com>
2026-06-10 07:38:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Video Tracker Running"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Video Tracker Running"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
